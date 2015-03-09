44b03e18ce4f6d6dbc1dbea32578ac993c5d6aac
msingh7 <divmeh@hotmail.com>
2015-03-09 10:41:53 +0530

    updates in routes

--- app/routes.php
+++ app/routes.php
@@ -78,15 +78,12 @@ Route::post('admin/promotional-artist-index','HostelsController@promotional_arti
 Route::get('admin/promotional-artist-add','HostelsController@add_promotional_artist');
 Route::post('admin/promotional-artist-save','HostelsController@save_promotional_artist');
 Route::get('admin/promotional-artist-index-all','HostelsController@promotional_artist_index_all');
-<<<<<<< HEAD
+
 
 Route::get('admin/edit-promotional-artist/{id}','HostelsController@edit_promotional_artist');
 Route::post('admin/update-promotional-artist','HostelsController@update_promotional_artist');
 Route::get('admin/delete-promotional-artist','HostelsController@delete_promotional_artist');
-=======
-Route::get('admin/edit-promotional-artist/{id}','HostelsController@promotional_artist_edit');
-Route::post('admin/promotional-artist-update','HostelsController@promotional_artist_update');
->>>>>>> 637ec499bc07bca703fffd6b608b3dfb85ca0953
+
 
 
 
